bc1bc65c16c4b6ec92df7ad81bf674599e7ac193
Ungern <giuseppe.pagliuca@gmail.com>
2015-11-13 22:31:50 +0100

    Added the tpl extraction

--- pyfas/test/test_tpl.py
+++ pyfas/test/test_tpl.py
@@ -4,4 +4,16 @@ def test_init():
     tpl = Tpl("FC1_rev01.tpl")
     assert tpl.fname == "FC1_rev01.tpl"
 
+def test_attributes():
+    tpl = Tpl("FC1_rev01.tpl")
+    assert tpl._attibutes['CATALOG'] == 331
+    assert tpl._attibutes['data_idx'] == 421
+
+def test_extraction():
+    tpl = Tpl("FC1_rev01.tpl")
+    time, data, metadata = tpl.extract(3)
+    assert int(time[1]) == 60
+    assert data[0] == 9.973410e6
+    assert 'Pressure' in metadata
+
 
--- pyfas/tpl.py
+++ pyfas/tpl.py
@@ -1,6 +1,38 @@
-class Tpl:
-    def __init__(self, fname):
-        self.fname = fname
+import numpy as np
 
-    def _define_attributes(self):
+class Tpl:
+    """
+    Data extraction for tpl files (OLGA >= 6.0)
+    """
+    def __init__(self, fname):
+        """
+        Initialize the tpl attributes
+        """
+        self.fname = fname
+        self._attibutes = {}
+        with open(self.fname) as fobj:
+            for idx, line in enumerate(fobj):
+                if 'CATALOG' in line:
+                    self._attibutes['CATALOG'] = idx
+                    self._attibutes['nvars'] = idx+1
+                if 'TIME SERIES' in line:
+                    self._attibutes['data_idx'] = idx
+                    break
+
+    def find_varaible(self, pattern=''):
         pass
+
+    def extract(self, variable_idx):
+        """
+        Extract a specific varaible
+        """
+        time, data = np.loadtxt(self.fname,
+                                skiprows=self._attibutes['data_idx']+1,
+                                unpack=True,
+                                usecols=(0, variable_idx))
+        with open(self.fname) as fobj:
+            for idx, line in enumerate(fobj):
+                if idx == 1 + variable_idx+self._attibutes['CATALOG']:
+                    metadata = line
+                    break
+        return time, data, metadata
