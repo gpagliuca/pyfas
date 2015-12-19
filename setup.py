e32eabd9981da5597dea1b8d045f2973bdb243e9
Ungern <giuseppe.pagliuca@gmail.com>
2015-12-19 01:40:05 +0100

    0.1.3

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@ from distutils.core import setup
 
 setup(
     name='pyfas',
-    version='0.1.2',
+    version='0.1.3',
     author='Giuseppe Pagliuca',
     author_email='giuseppe.pagliuca@gmail.com',
     packages=['pyfas'],
     url='https://github.com/gpagliuca/pyfas.git',
     license='LICENSE.txt',
     description='Toolbox for FA engineers',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'ipython',
+        'jupyter',
+        'xlrd',
+        'pandas',
+        'matplotlib',
+        'pytest',
+    ]
 )
