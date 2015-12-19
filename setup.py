12689b1c18b053b80e7aa55f82bd3d97a0ce235a
Ungern <giuseppe.pagliuca@gmail.com>
2015-12-19 01:05:47 +0100

    bugfixing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyfas',
-    version='0.1.0',
+    version='0.1.1',
     author='Giuseppe Pagliuca',
     author_email='giuseppe.pagliuca@gmail.com',
     packages=['pyfas'],
