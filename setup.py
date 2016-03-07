e9e2815212066a35254c78ec2ab550b1c4d611a8
--global <giuseppe.pagliuca@gmail.com>
2016-03-07 19:44:10 +0100

    v0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyfas',
-    version='0.1.4',
+    version='0.1.5',
     author='Giuseppe Pagliuca',
     author_email='giuseppe.pagliuca@gmail.com',
     packages=['pyfas'],
@@ -18,7 +18,7 @@ setup(
         'pandas',
         'matplotlib',
         'pytest',
-        'quantities'
-        'openpyxl'
+        'quantities',
+        'openpyxl',
     ]
 )
