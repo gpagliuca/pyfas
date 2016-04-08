e68b9ca4db5a34094b07e81d4611613f8f0b5549
peppe <giuseppe.pagliuca@gmail.com>
2016-04-08 13:18:05 +0200

    v 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyfas',
-    version='0.1.6',
+    version='0.1.7',
     author='Giuseppe Pagliuca',
     author_email='giuseppe.pagliuca@gmail.com',
     packages=['pyfas'],
