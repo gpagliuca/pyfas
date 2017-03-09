ab6c28e773238123c85c0c73c2c95a8a23661b2e
Giuseppe Pagliuca <giuseppe.pagliuca@gmail.com>
2017-03-09 10:54:12 +0100

    Update gists.py
    added <empty> to the bad values from unisim

--- pyfas/gists.py
+++ pyfas/gists.py
@@ -20,7 +20,7 @@ def unisim_csv(fname):
                      skiprows=10,
                      index_col=0,
                      na_values=('Shutdown', 'Bad',
-                                'I/O Timeout', 'Scan Timeout'))
+                                'I/O Timeout', 'Scan Timeout', ' <empty> '))
     df.columns = headers
     df.units = UoMs
     return df
