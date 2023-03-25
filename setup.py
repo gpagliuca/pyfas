e064d61a0a0649cf0bea8b3e970d4ecf9005895e
Giuseppe Pagliuca <giuseppe.pagliuca@gmail.com>
2023-03-25 22:39:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfas",
-    version="0.4.1",
+    version="0.4.2",
     author="Giuseppe Pagliuca",
     author_email="giuseppe.pagliuca@gmail.com",
     description="Toolbox for Flow Assurance engineers",
