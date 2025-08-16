f85ba62b5c406b4db3ad69e61f0bbabbef9a762a
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-08-16 20:18:01 +0300

    first push

---
+++ pyfas/espdb.py
@@ -0,0 +1,33 @@
+class ESPDBFile:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_espdb(path, esp_vers, esp_label, esp_model, freq, min_rate:float, max_rate:float,
+                flow_rate:list, head_per_stage:list, power:list, rate_units = 'm3/sec'):
+
+        strings_to_txtfile = []
+        if len(flow_rate) == len(head_per_stage) == len(power):
+            for i in range(len(flow_rate)):
+                strings_to_txtfile.append(str(f'{esp_vers},{esp_label},{esp_model},{freq},Calibration,Hz,' + \
+                                            f'{min_rate/86400},VolumeFlow,{rate_units},{max_rate/86400},VolumeFlow,{rate_units},' +\
+                                            f'{flow_rate[i]/86400},VolumeFlow,{rate_units},{head_per_stage[i]},Zlength,m,{power[i] * 0.00135962},Power,hp\n'))
+
+        else:
+            raise ValueError('Длины переданных массивов не совпадают')
+        # здесь будем формировать список из строк, который надо занести в .txt
+
+        with open(path, 'w+') as txtfile:
+        # здесь уже открываем файл и пишем в него
+            txtfile.write('version,label,model,frequency,type,units,min_rate,type2,units3,max_rate,type4,units5,flow_rate,type6,units7,head_per_stage,type8,units9,power,type10,units11\n')
+            txtfile.writelines(strings_to_txtfile)
+            txtfile.close()
+
+    
+
+if __name__ == '__main__':
+    esp = ESPDBFile()
+    esp.create_espdb('espdb_w200.txt',1,'LEMAZ','ESP5A-160',48.5,140,250,
+             flow_rate=[0.488, 44.61, 85.89, 145.2, 168.8, 202.7, 226.3, 245.5, 290.8, 357.1],
+             head_per_stage=[5.794, 5.833, 5.493, 5.225, 5.137, 4.806, 4.443, 4.092, 2.554, 0.446],
+             power=[88.96, 110.4,128.9,151.8,159.5,170.7, 176.3, 176.7, 171.3,164.1])
--- pyfas/ppl.py
+++ pyfas/ppl.py
@@ -6,9 +6,11 @@ import os
 import re
 import pandas as pd
 import numpy as np
-
+from easydict import EasyDict
+from pprint import pprint
 
 class Ppl:
+    
     """
     Data extraction for ppl files (OLGA >= 6.0)
     """
@@ -54,12 +56,19 @@ class Ppl:
             branch = branch_raw.replace("\'", '').replace("\n", '')
             self.extract_geometry(branch, branch_idx+2)
 
+        self._find_all_branches()
+        self._find_all_branches_and_vars()
+        self._ppl_to_dict()
+        self.create_df_dict()
+
+
     def _time_series(self):
         with open(self.abspath) as fobj:
             self.time = []
             for line in fobj.readlines()[1+self._attributes['data_idx']::
                                          self._attributes['nvar']+1]:
                 self.time.append(float(line))
+
 
     def filter_data(self, pattern=''):
         """
@@ -74,9 +83,11 @@ class Ppl:
                     filtered_profiles[idx-self._attributes['CATALOG']-1] = line
         return filtered_profiles
 
+
     def _define_branch(self, variable_idx):
-        return re.findall(r"'(.*)'", \
+        return re.findall(r"'(.+?)'", \
                           self.profiles[variable_idx])[2]
+
 
     def extract_geometry(self, branch, branch_begin):
         """
@@ -97,6 +108,7 @@ class Ppl:
         xy_geo = raw_geometry
         self.geometries[branch] = (xy_geo[:int(len(xy_geo)/2)],
                                    xy_geo[int(len(xy_geo)/2):])
+
 
     def extract(self, variable_idx):
         """
@@ -124,6 +136,7 @@ class Ppl:
         else:
             self.data[variable_idx][0] = np.array(x_no_st)
 
+
     def to_excel(self, *args):
         """
         Dump all the data to excel, fname and path can be passed as args
@@ -149,3 +162,130 @@ class Ppl:
             mylabel = "{} - {} - {}".format(myvar, br_label, unit)
             data_df.to_excel(xl_file, sheet_name=mylabel)
         xl_file.save()
+
+
+    def _find_all_branches(self):
+
+        def extract_branch_value(line):
+            parts = line.split("'BRANCH:'")  # Разделяем строку по 'BRANCH:'
+            if len(parts) > 1:
+                branch_part = parts[1].strip()  # Берем часть после 'BRANCH:' и убираем пробелы
+                # Извлекаем значение в кавычках (первое после 'BRANCH:')
+                branch_value = branch_part.split("'")[1] if "'" in branch_part else None
+                return branch_value
+            return None
+
+
+        branch_values = [extract_branch_value(line) for line in list(self.profiles.values())]
+
+        self.__all_branches = list(set(branch_values))
+
+
+    def _find_all_branches_and_vars(self):
+        branches_and_props = {}
+        for branch in self.__all_branches:
+            props = {}
+            cur_branch_dict = self.filter_data(branch)
+            for id in cur_branch_dict.keys():
+                props[id] = cur_branch_dict[id].split(' ')[0].strip()
+
+            branches_and_props[branch] = props
+
+        self.__branches_and_props = branches_and_props
+
+
+    def _ppl_to_dict(self) -> dict:
+        '''Method generate json with dataframes from .ppl
+        
+        Return: dict
+        '''
+        main_dict = {}
+        for branch in self.__all_branches:
+            property_dict = {}
+            for property_id in self.__branches_and_props[branch].keys():
+                self.extract(property_id)
+                property_dict[self.__branches_and_props[branch][property_id]] = list(self.data.values())
+                self.data = {}
+            main_dict[branch] = property_dict
+        self.data = main_dict
+
+
+    def create_df(self, branch, prop):
+        df = pd.DataFrame(self.data[branch][prop][0][1]).T
+        df.columns = self.time
+        df.index = self.data[branch][prop][0][0]
+        return df
+
+
+    def _safe_easydict(self, data):
+        """Преобразует словарь в EasyDict с автоматическим исправлением невалидных ключей.
+        
+        Обрабатывает:
+        - Ключи, начинающиеся с цифр (добавляет префикс)
+        - Ключи с точками и другими недопустимыми символами (заменяет на _)
+        - Сохраняет вложенную структуру
+        """
+        def to_valid_key(key):
+            """Внутренняя функция для преобразования ключа в валидный идентификатор"""
+            key = str(key)
+            # Добавляем префикс, если ключ начинается с цифры
+            if re.match(r'^\d', key):
+                key = f"BRANCH_{key}"
+            # Заменяем все недопустимые символы на _
+            key = re.sub(r'[^a-zA-Z0-9_]', '_', key)
+            # Убедимся, что ключ не стал пустым после преобразований
+            if not key:
+                key = "EMPTY_KEY"
+            return key
+
+        if not isinstance(data, dict):
+            return data
+            
+        new_data = {}
+        for key, value in data.items():
+            safe_key = to_valid_key(key)
+            # Рекурсивная обработка вложенных словарей
+            new_data[safe_key] = self._safe_easydict(value) if isinstance(value, dict) else value
+        
+        return EasyDict(new_data)
+
+
+    def create_df_dict(self):
+        df_dict = {}
+        for branch in self.__all_branches:
+            branch_dict = {}
+            for var in self.__branches_and_props[branch].values():
+                branch_dict[var] = self.create_df(branch=branch, prop=var)
+
+            df_dict[branch] = branch_dict
+        print('create_df_dict is working')
+        self.data_in_df = self._safe_easydict(df_dict)
+
+
+    @property
+    def branches(self):
+        print(self.__all_branches)
+
+
+    @property
+    def timesteps(self):
+        print(self.time)
+
+
+    @property
+    def info(self):
+        print(f'Timesteps: {self.time}')
+        print('===')
+        print(f'Number of timesteps: {len(self.time)}')
+        print('===')
+        print(f'Branches:{self.__all_branches}')
+        print('====')
+        print('Props:')
+        pprint( self.__branches_and_props, compact= True, width= 200)
+
+
+
+if __name__ == '__main__':
+    ppl_test = Ppl(r'C:\Users\user\Desktop\pyfas\pyfas\pyfas\test.ppl')
+    ppl_test.info
+    #print(ppl_test.data_in_df)
